e7af19fc7c7664c777cf1ed7d11d789e7e4bf644
slendchat <slendchat@gmail.com>
2023-09-21 01:15:09 -0700

    a

--- src.c
+++ src.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("hello_world\n");
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            printf("*");
+        }
+        
+    }
+    
+    printf("\nhello_world\n");
     return 1;
 }
