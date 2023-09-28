b1e65e05352143fb383c13974ea72fb5476c219a
slendchat <slendchat@gmail.com>
2023-09-28 00:36:08 -0700

    task_1

--- src.c
+++ src.c
@@ -11,12 +11,16 @@ int main()
     int* Ipointer = &i;
     number = *Ipointer;
 
-    Ipointer = 15;
-    *Ipointer = arr;
-
-    Ipointer = arr +3*sizeof(int);
+    *Ipointer = 15;
+    printf("%d\n",*Ipointer);
+    Ipointer = arr;
+    printf("%d\n",*Ipointer);
+    
+    Ipointer = Ipointer + 2;
+    printf("%d\n",*Ipointer);
     arr[0]=5;
-    Ipointer = 6;
+    *Ipointer = 6;
+    printf("%d\n",*Ipointer);
 
     return 0;
 }
