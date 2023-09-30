17cbdc17e55d6193a78747242f9bc61726093e50
Artur <slendchat@gmail.com>
2023-09-30 10:44:09 +0300

    test

--- src.c
+++ src.c
@@ -2,27 +2,6 @@
 
 int main()
 {
-    int i = 69;
-    int number = 72;
-    i+=72;
-
-    int arr[3] = {0};
-
-    int* Ipointer = &i;
-    number = *Ipointer;
-
-    *Ipointer = 15;
-    printf("%d\n",*Ipointer);
-    Ipointer = arr;
-    printf("%d\n",*Ipointer);
-    
-    Ipointer = Ipointer + 2;
-    printf("%d\n",*Ipointer);
-    arr[0]=5;
-    *Ipointer = 6;
-    printf("%d\n",*Ipointer);
-
-
-    printf("test test test");
+    printf("\n\n\ntest test test\n\n\n");
     return 0;
 }
