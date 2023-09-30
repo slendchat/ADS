3939232a5af706302c780f859a15297c71d548ff
Artur <slendchat@gmail.com>
2023-09-30 10:50:44 +0300

    test_folders

---
+++ inc/head.h
@@ -0,0 +1,3 @@
+#pragma once
+
+char* str = "hello_world\n\0";
--- src/src.c
+++ src/src.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
+#include "../inc/head.h"
 int main()
 {
     printf("\n\n\ntest test test\n\n\n");
+    printf("%s",str);
     return 0;
 }
